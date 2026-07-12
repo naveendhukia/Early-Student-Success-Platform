851118f0fe47cad357b690bd016b05df72077b32
naveendhukia <24B1832@iitb.ac.in>
2026-07-12 13:21:13 +0530

    Add logging tests in test_logger.py

---
+++ test_logger.py
@@ -0,0 +1,9 @@
+from src.utils.logger import logger
+
+logger.info("Logger initialized.")
+
+logger.success("Everything is working!")
+
+logger.warning("This is a warning.")
+
+logger.error("This is an error.")
